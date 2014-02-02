58773af7b694cb22db6e7d55698a54228b91a7d3
Tim Savage <tim.savage@poweredbypenguins.org>
2014-02-03 00:39:46 +1100

    Correct the resolver to use correct attribute names for django models

--- baldr/models.py
+++ baldr/models.py
@@ -7,6 +7,6 @@ from odin.mapping import FieldResolverBase
 class ModelFieldResolver(FieldResolverBase):
     """Field resolver for Django Models"""
     def get_field_dict(self):
-        return {f.attr_name: f for f, _ in self.obj._meta.fields}
+        return {f.attname: f for f in self.obj._meta.fields}
 
 registration.register_field_resolver(ModelFieldResolver, models.Model)
