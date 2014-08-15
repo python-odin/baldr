1d17280e25c5f7404ed3bb2a628d3ed6be902f19
tims <tims@sakurasky.com>
2014-08-15 15:52:15 +1000

    Added initial iteration of model_resource_factory, this is a set of tools for automatically generating resources from django models

--- baldr/models.py
+++ baldr/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from odin import registration, Resource
-from odin.mapping import FieldResolverBase
+from odin import registration
+import odin
+from odin.mapping import FieldResolverBase, mapping_factory
 
 
 class ModelFieldResolver(FieldResolverBase):
@@ -12,3 +13,96 @@ class ModelFieldResolver(FieldResolverBase):
         return {f.attname: f for f in self.obj._meta.fields}
 
 registration.register_field_resolver(ModelFieldResolver, models.Model)
+
+
+class ModelResourceMixin(odin.Resource):
+    class Meta:
+        abstract = True
+
+    _model = None
+
+    def save(self, context=None, save=True):
+        """
+        Save this resource instance to the database.
+        """
+        model = self.convert_to(self._model, context)
+        if save:
+            model.save()
+        return model
+
+
+MODEL_FIELD_MAP = [
+    # (Model Field, Odin Field, attribute mappings)
+    (models.DateTimeField, odin.DateTimeField, {}),
+    (models.DateField, odin.DateField, {}),
+    (models.TimeField, odin.TimeField, {}),
+    (models.URLField, odin.UrlField, {}),
+    (models.IntegerField, odin.IntegerField, {}),
+    (models.FloatField, odin.FloatField, {}),
+    (models.BooleanField, odin.BooleanField, {}),
+    (models.CharField, odin.StringField, {}),
+]
+
+
+def field_factory(model_field):
+    """
+    Return an equivalent odin field from a Django model field.
+    :param model_field:
+    :return:
+    """
+    for mf, of, m in MODEL_FIELD_MAP:
+        if isinstance(model_field, mf):
+            return of()
+
+
+def model_resource_factory(model, base_resource=odin.Resource, resource_mixins=None, module=None,
+                           exclude_fields=None, include_fields=None, generate_mappings=True, return_mappings=False):
+    """
+    Factory method for generating a resource from a existing Django model.
+
+    Usage::
+
+        class Person(models.Model):
+            name = models.CharField(max_length=50)
+            age = models.IntegerField()
+
+        PersonResource = model_resource_factory(Person)
+
+    :param model: The Django model to generate resource from.
+    :param base_resource: Base resource to extend from; default is ``odin.Resource``.
+    :param resource_mixins: Any additional mixin resources; default ``baldr.models.ModelResourceMixin``.
+    :param module: Module you want the class to be a member of; default is ``model.__module__``.
+    :param exclude_fields: Any fields that should be excluded from the resource.
+    :param include_fields: Explicitly define what fields that should be included on the resource.
+    :param generate_mappings: Generate mappings between resource and model (in both directions).
+    :param return_mappings: Return the mappings along with the model resource (returns a
+        tuple(Resource, ForwardMapping, ReverseMapping).
+
+    """
+    bases = tuple(resource_mixins or [] + [ModelResourceMixin, base_resource])
+    attrs = {}
+    model_opts = model._meta
+
+    # Append fields
+    exclude_fields = exclude_fields or []
+    for mf in model_opts.fields:
+        if mf.attname in exclude_fields:
+            continue
+
+        # Create an odin version of the field.
+        field = field_factory(mf)
+        if field:
+            attrs[mf.attname] = field
+
+    # Setup other require attributes and create type
+    attrs['__module__'] = module or model.__module__
+    attrs['_model'] = model
+    resource_type = type(model_opts.object_name, bases, attrs)
+
+    # Generate mappings
+    forward_mapping, reverse_mapping = mapping_factory(model, resource_type) if generate_mappings else None, None
+
+    if return_mappings:
+        return resource_type, forward_mapping, reverse_mapping
+    else:
+        return resource_type
