12d78ef0f737ea156db90f00131f36c70b7faeef
tims <tims@sakurasky.com>
2014-08-15 17:55:32 +1000

    Bumped version to 0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='baldr',
-    version='0.4.5',
+    version='0.5',
     url='https://github.com/timsavage/baldr',
     license='LICENSE',
     author='Tim Savage',
