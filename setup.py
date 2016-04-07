37ac9c497e820aeeef4b7f4dd2ef624978a22d49
Tim Savage <tim.savage@poweredbypenguins.org>
2016-04-07 15:44:49 +1000

    Tweaks to setup

--- setup.py
+++ setup.py
@@ -1,35 +1,51 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-try:
-    long_description = open("README.rst").read()
-except IOError:
-    long_description = ""
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='baldr',
+
     version='0.8.dev0',
-    url='https://github.com/timsavage/baldr',
-    license='LICENSE',
-    author='Tim Savage',
-    author_email='tim.savage@poweredbypenguins.org',
+
     description='Odin integration to Django',
     long_description=long_description,
-    packages=find_packages(exclude=("django_test_runner",)),
-    install_requires=['six', 'odin>=0.8', 'django>=1.7'],
+
+    url='https://github.com/python-odin/baldr',
+
+    author='Tim Savage',
+    author_email='tim@savage.company',
+
+    license='BSD',
 
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Framework :: Django',
+
         'Intended Audience :: Developers',
+
         'License :: OSI Approved :: BSD License',
+
+        'Framework :: Django',
         'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
+
+    packages=find_packages(exclude=("django_test_runner",)),
+    install_requires=['six', 'odin>=0.8.1', 'django>=1.7'],
 )
