6413de0b87722957e5a49bd441b18dcb934864f5
Tim Savage <tim@savage.company>
2017-06-02 01:15:10 +1000

    Migrated to PBR

--- setup.py
+++ setup.py
@@ -1,51 +1,8 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+#!/usr/bin/env python
+from setuptools import setup
 
 setup(
-    name='baldr',
-
-    version='0.8.dev0',
-
-    description='Odin integration to Django',
-    long_description=long_description,
-
-    url='https://github.com/python-odin/baldr',
-
-    author='Tim Savage',
-    author_email='tim@savage.company',
-
-    license='BSD',
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-
-        'Intended Audience :: Developers',
-
-        'License :: OSI Approved :: BSD License',
-
-        'Framework :: Django',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-
-    packages=find_packages(exclude=("django_test_runner",)),
-    install_requires=['six', 'odin>=0.8.1', 'django>=1.7'],
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
+
